ba5ebecc06f22ba045af22e5cd478ab0f32eaeac
Petar Petrov <otcrovenie_313@abv.bg>
2020-04-12 17:09:15 +0300

    Create CofirmDelete function in site.js

--- Web/MyFitScope.Web/wwwroot/js/site.js
+++ Web/MyFitScope.Web/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function ConfirmDelete(confirmMessage) {
+    if (!confirm(confirmMessage)) {
+        event.preventDefault();
+    }
+}
