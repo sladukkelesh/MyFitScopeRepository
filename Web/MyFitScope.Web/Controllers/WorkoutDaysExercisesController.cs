cf474eaae8e8ff18b61b27d59c4c204afa53eda8
Petar Petrov <otcrovenie_313@abv.bg>
2020-04-19 21:39:01 +0300

    Create Styles for Sidebar, CurrentWorkout, Edit Workout Day, Custom Form Template

--- Web/MyFitScope.Web/Controllers/WorkoutDaysExercisesController.cs
+++ Web/MyFitScope.Web/Controllers/WorkoutDaysExercisesController.cs
@@ -12,14 +12,28 @@
     public class WorkoutDaysExercisesController : Controller
     {
         private readonly IWorkoutDaysExercisesService workoutDayExerciseService;
+        private readonly IWorkoutDaysService workoutDayService;
 
-        public WorkoutDaysExercisesController(IWorkoutDaysExercisesService workoutDayExerciseService)
+        public WorkoutDaysExercisesController(IWorkoutDaysExercisesService workoutDayExerciseService, IWorkoutDaysService workoutDayService)
         {
             this.workoutDayExerciseService = workoutDayExerciseService;
+            this.workoutDayService = workoutDayService;
         }
 
         public async Task<IActionResult> AddExerciseToWorkoutDay(AddExerciseToWorkoutDayInputModel input)
         {
+            var exerciseAlreadyIn = this.WorkoutDayAlreadyContainsExercise(input.WorkoutDayId, input.ExerciseId);
+
+            if (!string.IsNullOrEmpty(exerciseAlreadyIn))
+            {
+                this.ModelState.AddModelError(string.Empty, exerciseAlreadyIn);
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.RedirectToAction("Edit", "WorkoutDays", new { workoutDayId = input.WorkoutDayId });
+            }
+
             await this.workoutDayExerciseService.AddExerciseToWorkoutDayAsync(input.ExerciseId, input.WorkoutDayId);
 
             return this.RedirectToAction("Edit", "WorkoutDays", new { workoutDayId = input.WorkoutDayId });
@@ -30,6 +44,21 @@
             var workoutDayId = await this.workoutDayExerciseService.RemoveExerciseFromWorkoutDayAsync(exerciseId);
 
             return this.RedirectToAction("Edit", "WorkoutDays", new { workoutDayId });
+        }
+
+        private string WorkoutDayAlreadyContainsExercise(string workoutDayId, string exerciseId)
+        {
+            var workoutDay = this.workoutDayService.GetWorkoutDayById(workoutDayId);
+
+            if (workoutDay.Exercises.Any(e => e.Id == exerciseId))
+            {
+                var exerciseName = workoutDay.Exercises.FirstOrDefault(e => e.Id == exerciseId).Name;
+                var day = workoutDay.WeekDayTitle;
+
+                return day + " already contains exercise with name " + exerciseName;
+            }
+
+            return null;
         }
     }
 }
