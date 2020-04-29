298b006fc4e9802a76a899adc57b6fed5361a89f
Petar Petrov <otcrovenie_313@abv.bg>
2020-04-29 21:10:15 +0300

    Add validations for Progress Statistics, Fix WorkoutsListing "No Custom workouts"result

--- Web/MyFitScope.Web.ViewModels/Progresses/CreateStatisticInputViewModel.cs
+++ Web/MyFitScope.Web.ViewModels/Progresses/CreateStatisticInputViewModel.cs
@@ -7,19 +7,25 @@
     public class CreateStatisticInputViewModel
     {
         [Required]
-        [Range(20, double.MaxValue, ErrorMessage = "The Weight value must be greater than 20kg")]
+        [Range(20, double.MaxValue, ErrorMessage = "The Weight value must be greater than 20kg!")]
         public double Weight { get; set; }
 
+        [Range(0, double.MaxValue, ErrorMessage = "The Biceps value must be a positive number!")]
         public double? Biceps { get; set; }
 
+        [Range(0, double.MaxValue, ErrorMessage = "The Chest value must be a positive number")]
         public double? Chest { get; set; }
 
+        [Range(0, double.MaxValue, ErrorMessage = "The Stomach value must be a positive number")]
         public double? Stomach { get; set; }
 
+        [Range(0, double.MaxValue, ErrorMessage = "The Hips value must be a positive number")]
         public double? Hips { get; set; }
 
+        [Range(0, double.MaxValue, ErrorMessage = "The Thigh value must be a positive number")]
         public double? Thigh { get; set; }
 
+        [Range(0, double.MaxValue, ErrorMessage = "The Calf value must be a positive number")]
         public double? Calf { get; set; }
     }
 }
